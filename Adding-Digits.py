313a887a0b326ead0f58d3322ec1fb9e786b6671
Hudaku <hudaku@tamu.edu>
2022-02-04 15:13:15 -0600

    Add Digits from a two-digit number

---
+++ Adding-Digits.py
@@ -0,0 +1,8 @@
+two_digits = (input("Type a two digit number: "))
+
+digit1_str = two_digits[0] ; digit2_str = two_digits[1] #assigning var to each digit
+
+digit1_int = int(digit1_str) ; digit2_int = int(digit2_str) #Turning digits to integers
+
+print(digit1_int+digit2_int) #Adding both integers
+
