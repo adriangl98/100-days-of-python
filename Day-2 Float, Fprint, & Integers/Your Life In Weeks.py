142b1b36535634ca162e44aab68e798c070a090c
Adrian <hudaku@tamu.edu>
2022-02-05 14:48:13 -0800

    Added Information to Folder Lessons & Day 2

---
+++ Day-2 Float, Fprint, & Integers/Your Life In Weeks.py
@@ -0,0 +1,11 @@
+age = int(input("What is your age? "))
+
+age_remain = 90 - age
+
+month_remain = age * 12
+
+weeks_remain = age * 52
+
+days_remain = age_remain * 365 
+
+print(f"You have {days_remain} days, {weeks_remain} weeks, and {month_remain} months left")
