ce9d17c4b0e06b30a7d3ea800c712adaca73e942
Adrian <hudaku@tamu.edu>
2022-02-05 16:16:48 -0800

    Started Day 3

---
+++ Day 3 - Logic Operator/Day-3 Logical Operators.py
@@ -0,0 +1,5 @@
+from __future__ import print_function
+
+
+print("Welcome to Treasure Island. \nYour Mission is to find the treasure.")
+print("You're at a cross road. Where do you want to go? Type \"left\" or \"right\"")
